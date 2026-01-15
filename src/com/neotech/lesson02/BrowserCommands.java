6c42ac1d13a057fa24115141545a9bcaa02833b0
sbushaj <69921436+sbneotech@users.noreply.github.com>
2026-01-14 20:36:16 -0500

    Made changes on lesson02 package

--- src/com/neotech/lesson02/BrowserCommands.java
+++ src/com/neotech/lesson02/BrowserCommands.java
@@ -1,7 +1,6 @@
 package com.neotech.lesson02;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class BrowserCommands {
 	// 1. Open chrome browser
@@ -11,45 +10,7 @@ public class BrowserCommands {
 	// 5. Refresh current page
 	// 6. Verify url contains “Zillow”
 	public static void main(String[] args) throws InterruptedException {
-		// 1. Open chrome browser
-		WebDriver driver = new ChromeDriver();
-
-		// 2. Navigate to “https://www.zillow.com/”
-		// driver.get("https://www.zillow.com/"); -> .get does not keep browsing history
-
-		// 2nd way: use the .navigate().to() method
-		driver.navigate().to("https://www.zillow.com/"); // will maintain the browsing history
-
-		Thread.sleep(4000);
-
-		// maximize the window
-		driver.manage().window().maximize();
-		// driver.manage().window().fullscreen();
-
-		Thread.sleep(3000);
-
-		// 3. Navigate to “https://www.google.com/”
-		driver.navigate().to("https://www.google.com/");
-		Thread.sleep(3000);
-
-		// 4. Navigate back to Zillow Page
-		driver.navigate().back();
-		Thread.sleep(3000);
-
-		// 5. Refresh current page
-		driver.navigate().refresh();
-		Thread.sleep(3000);
-
-		// 6. Verify url contains “Zillow”
-		String url = driver.getCurrentUrl();
-		if (url.contains("zillow")) {
-			System.out.println("Test passed!");
-		} else {
-			System.out.println("Test failed!!!");
-			System.out.println("Current url: " + url);
-		}
-
-		driver.quit();
+	
 
 	}
 
--- src/com/neotech/lesson02/FacebookLogin.java
+++ src/com/neotech/lesson02/FacebookLogin.java
@@ -1,10 +1,5 @@
 package com.neotech.lesson02;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class FacebookLogin {
 	// go to https://facebook.com/
 	// fill out the username
@@ -12,25 +7,7 @@ public class FacebookLogin {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://facebook.com/");
-
-		// locate the email textbox
-
-		// create a WebElement object
-		WebElement emailBox = driver.findElement(By.name("email"));
-		// send email to the emailBox
-		emailBox.sendKeys("neo@tech.com");
-
-		Thread.sleep(2000);
-
-		// locate the password box
-		WebElement passwordBox = driver.findElement(By.name("pass"));
-		passwordBox.sendKeys("pass123!");
-
-		Thread.sleep(3000);
-
-		driver.quit();
+	
 
 	}
 
